34a343668a260ae23a084f811ce21196e59b2407
wagnermarques <wagnerdocri@gmail.com>
2026-04-29 22:16:13 -0300

    Mexemos no app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,19 @@
 const express = require("express");
+const clienteRoutes = require("./routes/clienteRoutes")
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+//rota raiz
+app.get("/", (req,res) => {
+    res.json({
+        mensagem: "Api de clientes",
+        versao:"1.0.0",
+        endpoints:{
+            listarTodos: "GET /clientes",
+            buscarPorId: "GET /clientes/:id"
+        }
+    })
+})
